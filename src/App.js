48e68d11f1f055e69b29b3cfab85190c5c061dae
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-04 20:44:44 +0300

    Map coordinates refresh pin list

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ import Map from "./components/Map/Map";
 const App = () => {
     const [places, setPlaces] = useState([]);
 
+    const [coordinates, setCoordinates] = useState({lat:0 ,lng: 0});
+    const [bounds, setBounds] = useState(null);
+
     useEffect(() => {
         getPlacesData()
         .then((data) => {
             console.log(data);
-            
+
             setPlaces(data);
         })
     },[]);
@@ -27,7 +30,11 @@ const App = () => {
                     <List />
                 </Grid>
                 <Grid item xs={12} md={8}>
-                    <Map />
+                    <Map 
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                    />
                 </Grid>
             </Grid>
         </>
