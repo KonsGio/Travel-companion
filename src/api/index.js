25ee41e388fd921ec6c4fd9194d3489b66b9b296
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-04 20:33:43 +0300

    Log/lat default values and connection to google api

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,28 @@
 import axios from "axios";
 
-const getPlacesData = async () => {
-    try {
-        const response = await axios.get();
+// url for api call at rapid api
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng';
 
+const options = {
+ 
+  params: {
+    bl_latitude: '11.847676',
+    tr_longitude: '12.838442',
+    bl_longitude:'109.095887',
+    tr_longitude:'109.149359'
+  },
+  headers: {
+    'X-RapidAPI-Key': 'e244614d31msh20c4eed92f7718bp119f4ajsn8fa3b523553d',
+    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+  }
+};
+
+export const getPlacesData = async () => {
+    try {
+        const {data:{data}} = await axios.get(URL, options);
+
+        return data;
     } catch (error){
-        
+        console.log(error)
     }
 }
